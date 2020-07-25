93514ae172b24c8c5e109b4d32ac0f2df1c7213c
JustoKochung <kochungjusto26@gmail.com>
2020-07-25 11:15:28 +0300

    modify html, js, and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
-var daysArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var akanM = {Sunday: "Kwasi", Monday: "Kwadwo", Tuesday: "Kwabena", Wednesday: "Kwaku", Thurday: "Yaw", Friday: "Kofi", Saturday: "Kwame"};
-var akanF = {Sunday: "Akosua", Monday: "Adwoa", Tuesday: "Abenaa", Wednesday: "Akua", Thurday: "Yaa", Friday: "Afua", Saturday: "Ama"};
 
-function userEntries() {
+
+function formFn() {
     var gender = document.getElementsByName("gender");
     var mdate = document.getElementById("day");
     var mmonth = document.getElementById("month");
@@ -11,106 +9,104 @@ function userEntries() {
     var i = 0;
 
     if (mdate.value == "" || mdate.value == null) {
+        // alert("Please Input date");
         document.getElementById("dob").innerHTML = "Day Required";
-        document.getElementById("dob").style.color = "";
-        mdate.style.border = "2px solid red";
+        document.getElementById("dob");
         return false;
     } else {
-        mdate.style.border = "";
         if (!isNaN(mdate.value)) {
             if (mdate.value <= 0 || mdate.value > 31) {
                 document.getElementById("dob").innerHTML = "Invalid date";
-                document.getElementById("dob").style.color = "red";
-                mdate.style.border = "2px solid red";
+                document.getElementById("dob");
                 return false;
             } else {
-                document.getElementById("dob").innerHTML = "";
-                document.getElementById("dob").style.color = "";
-                mdate.style.border = "2px solid green";
+                document.getElementById("dob").innerHTML = "valid";
+                document.getElementById("dob");
             }
         } else {
             document.getElementById("dob").innerHTML = "Day must be a Number";
-            document.getElementById("dob").style.color = "red";
-            mdate.style.border = "2px solid red";
+            document.getElementById("dob");
             return false;
 
         }
     }
     if (mmonth.value == "" || mmonth.value == null) {
+
+
+
         document.getElementById("mmonth").innerHTML = "Month Required";
-        document.getElementById("mmonth").style.color = "red";
-        mmonth.style.border = "2px solid red";
+        document.getElementById("mmonth");
         return false;
     } else {
         if (!isNaN(mmonth.value)) {
             if (mmonth.value <= 0 || mmonth.value > 12) {
                 document.getElementById("mmonth").innerHTML = "Invalid Month";
-                document.getElementById("mmonth").style.color = "red";
-                mmonth.style.border = "2px solid red";
+                document.getElementById("mmonth");
                 return false;
             } else {
-                document.getElementById("mmonth").innerHTML = "";
-                document.getElementById("mmonth").style.color = "";
-                mmonth.style.border = "2px solid green";
+                document.getElementById("mmonth").innerHTML = "valid";
+                document.getElementById("mmonth");
             }
         } else {
             document.getElementById("mmonth").innerHTML = "Month must be a Number";
-            document.getElementById("mmonth").style.color = "red";
-            mmonth.style.border = "2px solid red";
+            document.getElementById("mmonth");
             return false;
         }
     }
     if (myear.value == "" || myear.value == null) {
 
         document.getElementById("myear").innerHTML = "Year Required";
-        document.getElementById("myear").style.color = "red";
-        myear.style.border = "2px solid red";
+        document.getElementById("myear");
         return false;
     } else {
         if (!isNaN(myear.value)) {
             if (myear.value.length != 4) {
                 document.getElementById("myear").innerHTML = "Invalid Year";
-                document.getElementById("myear").style.color = "red";
-                myear.style.border = "2px solid red";
+                document.getElementById("myear");
                 return false;
             } else {
-                document.getElementById("myear").innerHTML = "";
-                document.getElementById("myear").style.color = "";
-                myear.style.border = "2px solid green";
+                document.getElementById("myear").innerHTML = "valid";
+                document.getElementById("myear");
             }
         } else {
             document.getElementById("myear").innerHTML = "Year must be a Number";
-            document.getElementById("myear").style.color = "";
-            myear.style.border = "2px solid red";
+            document.getElementById("myear");
             return false;
         }
     }
     while (!formValid && i < gender.length) {
         if (gender[i].checked) {
-            document.getElementById("legend").style.color = "";
-            document.getElementById("legend").innerHTML = "";
+            // mgender = gender[i].value;
+            document.getElementById("legend");
+            document.getElementById("legend").innerHTML = "valid";
             formValid = true;
         }
         i++;
     }
     if (!formValid) {
-        document.getElementById("legend").style.color = "";
+        document.getElementById("legend");
         return false;
     }
     return formValid;
-
 }
+
+//functions to get user details
 function getUserDetails() {
     var gender = document.getElementsByName("gender");
     var mdate = parseInt(document.getElementById("day").value);
     var mmonth = parseInt(document.getElementById("month").value);
     var myear = parseInt(document.getElementById("year").value);
+    // var mdate = 12;
+    // var mmonth = 12;
+    // var myear = 2012;
     var i = 0;
+
     while (i < gender.length) {
         if (gender[i].checked)
 
             mgender = gender[i].value;
         i++;
+
     }
     var userDetailsObj = {
         date: mdate,
@@ -119,24 +115,41 @@ function getUserDetails() {
         gender: mgender
     }
     return userDetailsObj;
+
 }
+//Arrays
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var akanM = {Sunday: "Kwasi", Monday: "Kwadwo", Tuesday: "Kwabena", Wednesday: "Kwaku", Thurday: "Yaw", Friday: "Kofi", Saturday: "Kwame"};
+var akanF = {Sunday: "Akosua", Monday: "Adwoa", Tuesday: "Abenaa", Wednesday: "Akua", Thurday: "Yaa", Friday: "Afua", Saturday: "Ama"};
+
+// function to get the day of the week
 function dayOfWkCal() {
     var userDetailsObj = getUserDetails();
     var day = userDetailsObj.date;
     var month = userDetailsObj.month;
     var year = userDetailsObj.year;
+
+    // alert("dd "+day+" mm "+month +" yyyy "+year);
+
     var a = Math.floor((14 - month) / 12);
     var y = year - a;
     var m = month + 12 * a - 2;
     dayOfWk = (day + y + Math.floor(y / 4) - Math.floor(y / 100) +
         Math.floor(year / 400) + Math.floor((31 * m) / 12)) % 7;
-    var myDay = daysArr[dayOfWk];
+
+    var myDay = days[dayOfWk];
+    // alert(myDay);
     return myDay;
+
 }
+// function to get Akan Names
 function getAkanName() {
     var userDetailsObj = getUserDetails();
     var gender = userDetailsObj.gender;
+    
     var dayOfBirth = dayOfWkCal();
+  
+
     if (gender === "male") {
         for (var akanKey in akanM) {
             if (akanM.hasOwnProperty(akanKey)) {
@@ -155,13 +168,22 @@ function getAkanName() {
     } else {
         alert("error");
     }
-    document.getElementById("results").innerHTML = "You're " + myAkanName + " and born on " + dayOfBirth;
+
+    // alert(myAkanName)
+
+    document.getElementById("results").innerHTML = "You were born on " + dayOfBirth + " and your Akan name is " + myAkanName;
+    
+
+
+
+
 }
 
-function aggregateMyFunctions() {
-    var isValid = userEntries();
+function overallFn() {
+    var isValid = formFn();
+
     if (!isValid) {
-        userEntries();
+        formFn();
         return false;
     } else {
         getUserDetails();
